0344faca1f522ffda35ab4eb952f63a45b2d6a0b
afshan688 <afshanshakeel688@gmail.com>
2024-10-08 17:37:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+//////Ch.... 26///////
+
+var roundedNum =
+Math.round(2.50);
+console.log(roundedNum);
+
+///////
+var a =
+Math.ceil(4.2);
+console.log(a);
+////
+var num =
+Math.floor(4.9);
+console.log(num);
+/////
+
+var num = 
+Math.round(6.4);
+console.log(num);
+/////
+
+var b = 
+Math.round(0.5);
+console.log(b);
+
+//////Part 2: /////
+var num =
+Math.floor(Math.random()*50)+ 1;
+console.log(num);
+
+////
+
+var a =
+Math.random();
+console.log(a);
+
+/////
+var dice = 
+Math.floor(Math.random()*6)+ 1;
+console.log(dice);
+////
+var toss =
+Math.random() <0.5 ? 'Head' :
+'Tail';
+console.log(toss);
+
+//using number//
+var str = '42';
+var num =
+parseInt(str);
+console.log(num);
